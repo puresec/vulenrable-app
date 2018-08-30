6fbcf41e1ba82eac707c0a3385855a31b3d78232
Yuri Shapira <yuri@puresec.io>
2018-08-30 10:56:41 +0300

    Remove debug prints

--- handler.js
+++ handler.js
@@ -4,7 +4,6 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 
 exports.convert = async (event) => {
-  console.log(JSON.stringify(event));
   let requestid = event.requestContext.requestId;
   let ip = event.requestContext.identity.sourceIp;
   let documentUrl = event.queryStringParameters.document_url;
@@ -18,10 +17,8 @@ exports.convert = async (event) => {
       }
     }
   ).promise();
-  console.log("persisted in DB");
+  
   let txt = child_process.execSync(`curl --silent -L ${documentUrl} | ./bin/catdoc -`).toString();
-  console.log("catdoc result: "+ txt);
-
 
   return {
     statusCode: 200,
